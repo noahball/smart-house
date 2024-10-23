4bf7d2a401b104a49ac53b151a11beb207ead728
Noah <noah@noahball.com>
2024-10-23 20:29:16 +1300

    Second Iteration

--- main.ino
+++ main.ino
@@ -1,53 +1,73 @@
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
+#include <NetworkClient.h>
+#include <WiFiAP.h>
 #include <DHT.h>
 
-const char *ssid = "Wi-Fi SSID";
-const char *password = "Wi-Fi Password";
+// Set these to your desired credentials.
+const char *ssid = "Smart Home";
+const char *password = "securepassword";
 
-WebServer server(80);
+NetworkServer server(80);
 DHT dht(26, DHT22);
 
-void handleRoot() {
-  char msg[1500];
-
-  snprintf(msg, 1500, "Hello world!");
-  server.send(200, "text/html", msg);
-}
-
-void setup(void) {
-
+void setup() {
   Serial.begin(115200);
+  Serial.println();
   dht.begin();
-  
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+  Serial.println("Configuring access point...");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  // You can remove the password parameter if you want the AP to be open.
+  // a valid password must have more than 7 characters
+  if (!WiFi.softAP(ssid, password)) {
+    log_e("Soft AP creation failed.");
+    while (1);
   }
-
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  if (MDNS.begin("esp32")) {
-    Serial.println("MDNS responder started");
-  }
-  server.on("/", handleRoot);
-
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
   server.begin();
-  Serial.println("HTTP server started");
+
+  Serial.println("Server started");
 }
 
-void loop(void) {
-  server.handleClient();
-  delay(2);//allow the cpu to switch to other tasks
+void loop() {
+  NetworkClient client = server.accept();  // listen for incoming clients
+
+  if (client) {                     // if you get a client,
+    Serial.println("New Client.");  // print a message out the serial port
+    String currentLine = "";        // make a String to hold incoming data from the client
+    while (client.connected()) {    // loop while the client's connected
+      if (client.available()) {     // if there's bytes to read from the client,
+        char c = client.read();     // read a byte, then
+        Serial.write(c);            // print it out the serial monitor
+        if (c == '\n') {            // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.printf("Hello, world!");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {  // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
 }
