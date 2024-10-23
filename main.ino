539404cbced32a3e26315a77ae8a88536dcd4318
Noah <noah@noahball.com>
2024-10-23 20:07:11 +1300

    First Iteration (Connects to existing access point)

---
+++ main.ino
@@ -0,0 +1,53 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <DHT.h>
+
+const char *ssid = "Wi-Fi SSID";
+const char *password = "Wi-Fi Password";
+
+WebServer server(80);
+DHT dht(26, DHT22);
+
+void handleRoot() {
+  char msg[1500];
+
+  snprintf(msg, 1500, "Hello world!");
+  server.send(200, "text/html", msg);
+}
+
+void setup(void) {
+
+  Serial.begin(115200);
+  dht.begin();
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+  server.on("/", handleRoot);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+  delay(2);//allow the cpu to switch to other tasks
+}
